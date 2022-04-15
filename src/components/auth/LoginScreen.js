8738671541cd561b1658df1693bec79710d69b3b
Anderson Ventosilla <andersonventosilla0@gmail.com>
2022-04-15 11:40:58 -0500

    248

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,23 +1,45 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { login } from '../../actions/auth'
+import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
+
+  const dispatch = useDispatch();
+
+  const [ formValues,handleInputChange ] = useForm({
+    email: 'andersonventosilla0@gmail.com',
+    password: '12345'
+  })
+
+  const { email,password } =formValues
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(12345,'taways'))
+  }
+
   return (
     <>
       <h3 className='auth__title'>Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type='text'
           placeholder='Email'
           name='email'
           className='auth__input'
           autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type='password'
           placeholder='password'
           name='password'
           className='auth__input'
+          value={password}
+          onChange={handleInputChange}
         />
         <button
           className='btn btn-primary btn-block'
